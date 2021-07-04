5b2d346222b0e0124bfc124149a41138af43e697
xxwmar <4265@163.com>
2021-07-04 10:57:02 +0800

    新增部分页面

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    title: 'Hello VitePress', // 网站标题
+    title: 'xWANG', // 网站标题
     description: 'Just playing around.', // 网站介绍
+    base: '/',
     themeConfig: {
         docsDir: 'docs',
         lastUpdated: '最后更新时间',
@@ -12,6 +13,19 @@ module.exports = {
                     { text: 'vue3入门', link: '/vue3' },
                     { text: 'demo1', link: '/course1'},
                     {text: 'demo2', link: '/course2'}
+                ]
+            },
+            { text: 'React.js', link: '/react-js' },
+            { text: '小程序', link: 'mini-program' },
+            {
+                text:
+                    '面试题库',
+                items: [
+                    { text: 'H5C3', link: '/interview/interview-h5c3' },
+                    { text: 'JavaScript', link: '/interview/interview-js' },
+                    { text: 'ES6新特性', link: '/interview/interview-es6'},
+                    { text: 'Vue.js', link: '/interview/interview-vue' }
+                    
                 ]
             },
             { text: '归档', link: '/pigeonhole' },
---
+++ docs/about.md
@@ -0,0 +1,16 @@
+::: tip xWANG：
+我白天是个程序员，晚上就是个有抱负的演员。这是我的网站。我住在天朝的帝都，有条叫做Jack的狗。
+:::
+
+
+
+
+
+
+
+<!-- 样式 -->
+<style>
+.container[data-v-7f5fbed2]{
+    max-width: 65rem;
+}
+</style>
